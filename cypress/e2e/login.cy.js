172f70c1f73ab3cefc9d2ca7fe528a50fbf30e8f
marvintatierra <marvin.tatierra@chemistwarehouse.com.au>
2024-11-22 14:18:43 +0800

    Added LoginTest

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+describe("Login Test", () => {
+  it("Successfully logs in", () => {
+    cy.visit("https://practicetestautomation.com/practice-test-login/");
+
+    cy.get("#username").type("student");
+    cy.get("#password").type("Password123");
+    cy.get("#submit").click();
+
+    // Verify successful login
+    cy.contains("Logged In Successfully").should("be.visible");
+  });
+});
